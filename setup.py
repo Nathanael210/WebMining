20782baa58c68ec59ab06a80a79f6317ceb596af
Victoria Zevallos <victoriazevallos@MacBook-Pro-de-Victoria.local>
2022-04-29 19:35:41 +0200

    Cookiecutter Data Science Template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Web Mining Project, Team 7',
+    author='Team_7',
+    license='MIT',
+)
