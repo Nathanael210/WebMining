cce4c4b83a7f76d2a1ea01756f492d5578757510
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-23 21:58:10 +0000

    Adding tarfile member sanitization to extractall()

--- src/data/process_metadata.py
+++ src/data/process_metadata.py
@@ -5,7 +5,29 @@ import pandas as pd
 filename = "WebMining/data/raw/cit-HepTh-abstracts.tar"
  
 with tarfile.open(filename, "r") as compressed_file:
-    compressed_file.extractall("extracted_tar_folder")
+    
+    import os
+    
+    def is_within_directory(directory, target):
+        
+        abs_directory = os.path.abspath(directory)
+        abs_target = os.path.abspath(target)
+    
+        prefix = os.path.commonprefix([abs_directory, abs_target])
+        
+        return prefix == abs_directory
+    
+    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+    
+        for member in tar.getmembers():
+            member_path = os.path.join(path, member.name)
+            if not is_within_directory(path, member_path):
+                raise Exception("Attempted Path Traversal in Tar File")
+    
+        tar.extractall(path, members, numeric_owner=numeric_owner) 
+        
+    
+    safe_extract(compressed_file, "extracted_tar_folder")
 
 years = listdir('WebMining/data/processed/extracted_tar_folder')
 all_papers = []
